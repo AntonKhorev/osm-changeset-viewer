82c2a0c2d87384bcb63c7c5f1501c8df7e4f02f8
Anton Khorev <tony29@yandex.ru>
2023-03-31 14:37:35 +0300

    Make basic changesets query

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import Net, {checkAuthRedirect, HashServerSelector} from './net'
-import {makeElement, makeDiv} from './util/html'
+import {toUserQuery} from './query-user'
+import {makeElement, makeDiv, makeLabel} from './util/html'
 import {PrefixedLocalStorage} from './util/storage'
+import {makeEscapeTag} from './util/escape'
 import serverListConfig from './server-list-config'
 
 const appName='osm-changeset-viewer'
@@ -27,7 +29,59 @@ async function main() {
 	net.serverSelector.installHashChangeListener(net.cx,()=>{})
 
 	$root.append(
-		makeElement('h1')()(`Changeset viewer`),
+		makeElement('h1')()(`Changeset viewer`)
+	)
+
+	if (net.cx) {
+		const cx=net.cx
+		const $userInput=makeElement('input')()()
+		$userInput.type='text'
+		const $form=makeElement('form')()(
+			makeDiv('major-input-group')(
+				makeLabel()(
+					`Username, URL or #id `,$userInput
+				)
+			),
+			makeDiv('major-input-group')(
+				makeElement('button')()(`Add user`)
+			)
+		)
+		$userInput.oninput=()=>{
+			const userQuery=toUserQuery(cx.server.api,cx.server.web,$userInput.value)
+			if (userQuery.userType=='invalid') {
+				$userInput.setCustomValidity(userQuery.message)
+			} else if (userQuery.userType=='empty') {
+				$userInput.setCustomValidity(`user query cannot be empty`)
+			} else {
+				$userInput.setCustomValidity('')
+			}
+		}
+		$form.onsubmit=async(ev)=>{
+			ev.preventDefault()
+			const userQuery=toUserQuery(cx.server.api,cx.server.web,$userInput.value)
+			if (userQuery.userType=='invalid' || userQuery.userType=='empty') return
+			const e=makeEscapeTag(encodeURIComponent)
+			let userParameter: string
+			if (userQuery.userType=='id') {
+				userParameter=e`user=${userQuery.uid}`
+			} else {
+				userParameter=e`display_name=${userQuery.username}`
+			}
+			const result=await cx.server.api.fetch(`changesets.json?${userParameter}`)
+			const json=await result.json()
+			console.log(json)
+		}
+		$root.append(
+			makeElement('h2')()(`Select users and changesets`),
+			$form
+		)
+	} else {
+		$root.append(
+			makeDiv('notice')(`Please select a valid server`)
+		)
+	}
+
+	$root.append(
 		...net.$sections
 	)
 }
---
+++ src/query-user.ts
@@ -0,0 +1,132 @@
+import type {ApiUrlLister, WebUrlLister} from './net'
+
+export interface UsernameQuery {
+	userType: 'name'
+	username: string
+}
+
+export interface UidQuery {
+	userType: 'id'
+	uid: number
+}
+
+export type ValidUserQuery = UsernameQuery | UidQuery
+
+export interface InvalidUserQuery {
+	userType: 'invalid'
+	message: string
+}
+
+export interface EmptyUserQuery {
+	userType: 'empty'
+}
+
+export type UserQuery = ValidUserQuery | InvalidUserQuery | EmptyUserQuery
+
+export function toUserQuery(apiUrlLister: ApiUrlLister, webUrlLister: WebUrlLister, value: string): UserQuery {
+	const s=value.trim()
+	if (s=='') return {
+		userType: 'empty'
+	}
+	if (s[0]=='#') {
+		let match: RegExpMatchArray | null
+		if (match=s.match(/^#\s*(\d+)$/)) {
+			const [,uid]=match
+			return {
+				userType: 'id',
+				uid: Number(uid)
+			}
+		} else if (match=s.match(/^#\s*\d*(.)/)) {
+			const [,c]=match
+			return {
+				userType: 'invalid',
+				message: `uid cannot contain non-digits, found ${c}`
+			}
+		} else {
+			return {
+				userType: 'invalid',
+				message: `uid cannot be empty`
+			}
+		}
+	}
+	if (s.includes('/')) {
+		const hosts=new Set<string>()
+		for (const urlString of [apiUrlLister.url,...webUrlLister.urls]) {
+			try {
+				const url=new URL(urlString)
+				hosts.add(url.host)
+			} catch {}
+		}
+		try {
+			const url=new URL(s)
+			if (!hosts.has(url.host)) {
+				let domainString=`was given ${url.host}`
+				if (!url.host) domainString=`no domain was given`
+				return {
+					userType: 'invalid',
+					message: `URL has to be of an OSM domain, ${domainString}`
+				}
+			}
+			const [,typeDir]=url.pathname.split('/',2)
+			if (typeDir=='user') {
+				const [,,userDir]=url.pathname.split('/',3)
+				if (!userDir) return {
+					userType: 'invalid',
+					message: `OSM user URL has to include username`
+				}
+				return {
+					userType: 'name',
+					username: decodeURIComponent(userDir)
+				}
+			} else if (typeDir=='api') {
+				const [,,apiVersionDir,apiCall,apiValue]=url.pathname.split('/',5)
+				if (apiVersionDir!='0.6' || apiCall!='user') return {
+					userType: 'invalid',
+					message: `OSM API URL has to be "api/0.6/user/..."`
+				}
+				const [uidString]=apiValue.split('.')
+				const uid=Number(uidString)
+				if (!Number.isInteger(uid)) return {
+					userType: 'invalid',
+					message: `OSM API URL has to include valid user id"`
+				}
+				return {
+					userType: 'id',
+					uid
+				}
+			} else {
+				return {
+					userType: 'invalid',
+					message: `OSM URL has to be either user page or user api link`
+				}
+			}
+		} catch {
+			return {
+				userType: 'invalid',
+				message: `string containing "/" character has to be a valid URL`
+			}
+		}
+	}
+	return {
+		userType: 'name',
+		username: s
+	}
+}
+
+export function makeUserQueryFromUserNameAndId(username: string|undefined|null, uid: number|undefined|null): UserQuery {
+	if (username!=null) {
+		return {
+			userType: 'name',
+			username
+		}
+	} else if (uid!=null && Number.isInteger(uid)) {
+		return {
+			userType: 'id',
+			uid
+		}
+	} else {
+		return {
+			userType: 'empty'
+		}
+	}
+}
