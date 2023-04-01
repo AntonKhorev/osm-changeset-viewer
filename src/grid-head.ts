52a664d8c24a0ad55881b9bda7386aef49942424
Anton Khorev <tony29@yandex.ru>
2023-04-02 02:08:52 +0300

    Add hash updates

--- src/grid-head.ts
+++ src/grid-head.ts
@@ -5,17 +5,24 @@ import ChangesetStream from './changeset-stream'
 import MuxChangesetStream from './mux-changeset-stream'
 import {makeElement, makeDiv, makeLabel} from './util/html'
 
+type GridUserEntry = {
+	query: ValidUserQuery
+	$user: HTMLElement
+}
+
 export default class GridHead {
+	private userEntries=[] as GridUserEntry[]
+	private $form=makeElement('form')()()
 	constructor(
-		cx: Connection,
-		$grid: HTMLElement,
-		receiveStream: (muxStream: MuxChangesetStream)=>void
+		private cx: Connection,
+		private $grid: HTMLElement,
+		sendUpdatedUserQueries: (userQueries: ValidUserQuery[])=>void,
+		private sendStream: (muxStream: MuxChangesetStream)=>void
 	) {
-		const userQueries: ValidUserQuery[] = []
 		const $userInput=makeElement('input')()()
 		$userInput.type='text'
 		$userInput.name='user'
-		const $form=makeElement('form')()(
+		this.$form.append(
 			makeDiv('major-input-group')(
 				makeLabel()(
 					`Username, URL or #id `,$userInput
@@ -25,37 +32,85 @@ export default class GridHead {
 				makeElement('button')()(`Add user`)
 			)
 		)
-		$grid.append($form)
+		$grid.append(this.$form)
 		$userInput.oninput=()=>{
-			const userQuery=toUserQuery(cx.server.api,cx.server.web,$userInput.value)
-			if (userQuery.type=='invalid') {
-				$userInput.setCustomValidity(userQuery.message)
-			} else if (userQuery.type=='empty') {
+			const query=toUserQuery(cx.server.api,cx.server.web,$userInput.value)
+			if (query.type=='invalid') {
+				$userInput.setCustomValidity(query.message)
+			} else if (query.type=='empty') {
 				$userInput.setCustomValidity(`user query cannot be empty`)
 			} else {
 				$userInput.setCustomValidity('')
 			}
 		}
-		$form.onsubmit=ev=>{
+		this.$form.onsubmit=ev=>{
 			ev.preventDefault()
-			const userQuery=toUserQuery(cx.server.api,cx.server.web,$userInput.value)
-			if (userQuery.type=='invalid' || userQuery.type=='empty') return
-			const $user=makeDiv('user')()
-			if (userQuery.type=='id') {
-				$user.append(`#${userQuery.uid}`)
-			} else {
-				$user.append(userQuery.username)
-			}
-			userQueries.push(userQuery)
-			$form.style.gridColumn=String(userQueries.length+1)
-			$form.before($user)
-			const streams=[] as ChangesetStream[]
-			for (const userQuery of userQueries) {
-				const stream=new ChangesetStream(cx,userQuery)
-				streams.push(stream)
-			}
-			const muxStream=new MuxChangesetStream(streams)
-			receiveStream(muxStream)
+			const query=toUserQuery(cx.server.api,cx.server.web,$userInput.value)
+			if (query.type=='invalid' || query.type=='empty') return
+			const $user=makeUserCard(query)
+			$user.style.gridColumn=String(this.userEntries.length+1)
+			this.userEntries.push({query,$user})
+			sendUpdatedUserQueries(this.userEntries.map(({query})=>query))
+			this.$form.style.gridColumn=String(this.userEntries.length+1)
+			this.$form.before($user)
+			this.openAndSendStream()
 		}
 	}
+	receiveUpdatedUserQueries(userQueries: ValidUserQuery[]): void {
+		const newUserEntries=[] as GridUserEntry[]
+		for (const [i,query] of userQueries.entries()) {
+			let entry=this.pickFromExistingUserEntries(query)
+			if (!entry) {
+				const $user=makeUserCard(query)
+				entry={query,$user}
+			}
+			entry.$user.style.gridColumn=String(i+1)
+			newUserEntries.push(entry)
+		}
+		this.$form.style.gridColumn=String(newUserEntries.length+1)
+		for (const {$user} of this.userEntries) {
+			$user.remove()
+		}
+		this.userEntries=newUserEntries
+		this.$grid.prepend(...newUserEntries.map(({$user})=>$user))
+		this.openAndSendStream()
+	}
+	private pickFromExistingUserEntries(query: ValidUserQuery): GridUserEntry|null {
+		for (const [i,entry] of this.userEntries.entries()) {
+			if (isSameQuery(query,entry.query)) {
+				this.userEntries.splice(i,1)
+				return entry
+			}
+		}
+		return null
+	}
+	private openAndSendStream(): void {
+		this.sendStream(
+			new MuxChangesetStream(
+				this.userEntries.map(({query})=>new ChangesetStream(this.cx,query))
+			)
+		)
+	}
+}
+
+function isSameQuery(query1: ValidUserQuery, query2: ValidUserQuery): boolean {
+	if (query1.type=='id') {
+		if (query2.type!=query1.type) return false
+		return query1.uid==query2.uid
+	} else if (query1.type=='name') {
+		if (query2.type!=query1.type) return false
+		return query1.username==query2.username
+	} else {
+		return false
+	}
+}
+
+function makeUserCard(query: ValidUserQuery): HTMLElement {
+	const $user=makeDiv('user')()
+	if (query.type=='id') {
+		$user.append(`#${query.uid}`)
+	} else {
+		$user.append(query.username)
+	}
+	return $user
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import Net, {checkAuthRedirect, HashServerSelector} from './net'
+import type {ValidUserQuery} from './osm'
 import More from './more'
 import writeToolbar from './toolbar'
 import makeNetDialog from './net-dialog'
@@ -7,11 +8,9 @@ import {installRelativeTimeListeners, makeDateOutputFromString} from './date'
 import serverListConfig from './server-list-config'
 import {makeElement, makeDiv, makeLink} from './util/html'
 import {PrefixedLocalStorage} from './util/storage'
-import {makeEscapeTag} from './util/escape'
+import {escapeHash, makeEscapeTag} from './util/escape'
 
 const appName='osm-changeset-viewer'
-
-const e=makeEscapeTag(encodeURIComponent)
 
 main()
 
@@ -33,7 +32,6 @@ async function main() {
 		serverList=>new HashServerSelector(serverList),
 		()=>{} // TODO event like bubbleEvent($root,'osmChangesetViewer:loginChange')
 	)
-	net.serverSelector.installHashChangeListener(net.cx,()=>{})
 	const $content=makeElement('main')()(
 		makeElement('h1')()(`Changeset viewer`)
 	)
@@ -45,12 +43,17 @@ async function main() {
 		const cx=net.cx
 		const more=new More()
 		const $grid=makeDiv('grid')()
-		const gridHead=new GridHead(cx,$grid,async(stream)=>{
+		const gridHead=new GridHead(cx,$grid,userQueries=>{
+			net.serverSelector.pushHashToHistory(
+				getHashFromUserQueries(userQueries)
+			)
+		},async(stream)=>{
 			for (const $changeset of $grid.querySelectorAll('.changeset')) {
 				$changeset.remove()
 			}
 			more.changeToLoadMore()
 			more.$button.onclick=async()=>{
+				const e=makeEscapeTag(encodeURIComponent)
 				more.changeToLoading()
 				const nUsersAndChangesets=await stream.fetch()
 				for (const [nUser,changeset] of nUsersAndChangesets) {
@@ -74,11 +77,48 @@ async function main() {
 			$grid,
 			more.$div
 		)
+		net.serverSelector.installHashChangeListener(net.cx,hostlessHash=>{
+			gridHead.receiveUpdatedUserQueries(
+				getUserQueriesFromHash(hostlessHash)
+			)
+		},true)
 	} else {
 		$content.append(
 			makeDiv('notice')(`Please select a valid server`)
 		)
+		net.serverSelector.installHashChangeListener(net.cx,()=>{})
 	}
 
 	writeToolbar($root,$toolbar,$netDialog)
+}
+
+function getUserQueriesFromHash(hash: string): ValidUserQuery[] {
+	const queries=[] as ValidUserQuery[]
+	for (const hashEntry of hash.split('&')) {
+		const match=hashEntry.match(/([^=]*)=(.*)/)
+		if (!match) continue
+		const [,k,v]=match
+		if (k=='user') {
+			queries.push({
+				type: 'name',
+				username: v
+			})
+		} else if (k=='uid') {
+			queries.push({
+				type: 'id',
+				uid: Number(v)
+			})
+		}
+	}
+	return queries
+}
+
+function getHashFromUserQueries(queries: ValidUserQuery[]): string {
+	return queries.map(query=>{
+		if (query.type=='name') {
+			return `user=`+escapeHash(query.username)
+		} else {
+			return `uid=`+escapeHash(String(query.uid))
+		}
+	}).join('&')
 }
