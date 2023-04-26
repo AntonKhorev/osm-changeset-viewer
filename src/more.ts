054f2728116b98b71a8966e04d40ebecf8cb0fe6
Anton Khorev <tony29@yandex.ru>
2023-04-26 15:51:00 +0300

    Add proper autoload state+option handling

--- src/more.ts
+++ src/more.ts
@@ -3,7 +3,8 @@ import {makeDiv, makeElement} from './util/html'
 export default class More {
 	$button=makeElement('button')()(`Load more`)
 	$div=makeDiv('more')(this.$button)
-	autoLoad=false
+	#autoLoad=false
+	private inLoadMoreState=false
 	intersectionObserver=new IntersectionObserver((entries)=>{
 		if (entries.length==0) return
 		if (!entries[0].isIntersecting) return
@@ -12,34 +13,51 @@ export default class More {
 	constructor() {
 		this.changeToNothingToLoad()
 	}
-	changeToNothingToLoad() {
-		this.intersectionObserver.disconnect()
+	get autoLoad(): boolean {
+		return this.#autoLoad
+	}
+	set autoLoad(value: boolean) {
+		this.#autoLoad=value
+		this.updateIntersectionObserver()
+	}
+	changeToNothingToLoad(): void {
+		this.inLoadMoreState=false
+		this.updateIntersectionObserver()
 		this.$div.hidden=true
 	}
-	changeToLoad() {
-		this.intersectionObserver.disconnect()
+	changeToLoad(): void {
+		this.inLoadMoreState=false
+		this.updateIntersectionObserver()
 		this.$div.hidden=false
 		this.$button.disabled=false
 		this.$button.textContent=`Load changesets`
 	}
-	changeToLoading() {
-		this.intersectionObserver.disconnect()
+	changeToLoading(): void {
+		this.inLoadMoreState=false
+		this.updateIntersectionObserver()
 		this.$div.hidden=false
 		this.$button.disabled=true
 		this.$button.textContent=`Loading changesets...`
 	}
-	changeToLoadedAll() {
-		this.intersectionObserver.disconnect()
+	changeToLoadedAll(): void {
+		this.inLoadMoreState=false
+		this.updateIntersectionObserver()
 		this.$div.hidden=false
 		this.$button.disabled=true
 		this.$button.textContent=`Loaded all changesets`
 	}
-	changeToLoadMore() {
-		if (this.autoLoad) {
-			this.intersectionObserver.observe(this.$button)
-		}
+	changeToLoadMore(): void {
+		this.inLoadMoreState=true
+		this.updateIntersectionObserver()
 		this.$div.hidden=false
 		this.$button.disabled=false
 		this.$button.textContent=`Load more changesets`
+	}
+	private updateIntersectionObserver(): void {
+		if (this.inLoadMoreState && this.autoLoad) {
+			this.intersectionObserver.observe(this.$button)
+		} else {
+			this.intersectionObserver.disconnect()
+		}
 	}
 }
