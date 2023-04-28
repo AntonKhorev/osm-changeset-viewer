efe03868c0eaf2039fbb5a73d9de78462ab032da
Anton Khorev <tony29@yandex.ru>
2023-04-28 17:26:51 +0300

    Split 'add user items' scenarios

--- src/worker/db-writer.ts
+++ src/worker/db-writer.ts
@@ -49,61 +49,96 @@ export class ChangesetViewerDBWriter extends ChangesetViewerDBReader {
 		})
 	}
 	/**
+	 * Store items if they were acquired as a side effect of some other operation, like changesets when getting user info
+	 *
 	 * @returns true if decided to add/update the scan
 	 */
-	addUserItems<T extends keyof UserItemDbRecordMap>(
-		type: T, uid: number, now: Date, items: UserItemDbRecordMap[T][], streamBoundary: StreamBoundary,
-		mode: 'toNewScan'|'toExistingScan'|'toNewOrExistingScan'
+	addUserItemsIfNoScan<T extends keyof UserItemDbRecordMap>(
+		type: T, uid: number, now: Date, items: UserItemDbRecordMap[T][], streamBoundary: StreamBoundary
 	): Promise<boolean> {
 		if (this.closed) throw new Error(`Database is outdated, please reload the page.`)
 		return new Promise((resolve,reject)=>{
 			const tx=this.idb.transaction([type,'userScans'],'readwrite')
-			tx.onerror=()=>reject(new Error(`Database error in addUserItems(): ${tx.error}`))
+			tx.onerror=()=>reject(new Error(`Database error in addUserItemsIfNoScan(): ${tx.error}`))
 			const handleScan=(scan: UserScanDbRecord)=>{
-				for (const item of items) {
-					tx.objectStore(type).put(item)
-					scan=addUserItemIdsAndDateToScan(scan,item.createdAt,[item.id])
-				}
-				if (streamBoundary.date) {
-					scan=addUserItemIdsAndDateToScan(scan,streamBoundary.date,[])
-				}
-				if (streamBoundary.isFinished) {
-					scan.endDate=now
-				} else {
-					delete scan.endDate
-				}
-				tx.objectStore('userScans').put(scan)
-				tx.oncomplete=()=>resolve(true)
+				const updatedScan=this.addUserItemsToScan(
+					now,items,streamBoundary,
+					tx.objectStore(type),scan
+				)
+				tx.objectStore('userScans').put(updatedScan)
 			}
-			const makeEmptyScan=():UserScanDbRecord=>({
-				uid,
-				type,
-				stash: 0,
-				items: {count:0},
-				beginDate: now,
-				empty: true
-			})
-			if (mode=='toNewScan') {
-				handleScan(makeEmptyScan())
-			} else {
-				const getScanRequest=tx.objectStore('userScans').get([uid,0,type])
-				getScanRequest.onsuccess=()=>{
-					let scan: UserScanDbRecord
-					if (getScanRequest.result==null) {
-						scan=makeEmptyScan()
-					} else {
-						if (mode=='toExistingScan') {
-							return resolve(false)
-						}
-						scan=getScanRequest.result
-					}
-					handleScan(scan)
+			const getScanRequest=tx.objectStore('userScans').get([uid,0,type])
+			getScanRequest.onsuccess=()=>{
+				if (getScanRequest.result==null) {
+					handleScan(makeEmptyScan(uid,type,now))
+					tx.oncomplete=()=>resolve(true)
+				} else {
+					resolve(false)
 				}
 			}
 		})
 	}
+	addUserItems<T extends keyof UserItemDbRecordMap>(
+		type: T, uid: number, now: Date, items: UserItemDbRecordMap[T][], streamBoundary: StreamBoundary,
+		forceNewScan: boolean
+	): Promise<void> {
+		if (this.closed) throw new Error(`Database is outdated, please reload the page.`)
+		return new Promise((resolve,reject)=>{
+			const tx=this.idb.transaction([type,'userScans'],'readwrite')
+			tx.onerror=()=>reject(new Error(`Database error in addUserItemsIfNoScan(): ${tx.error}`))
+			tx.oncomplete=()=>resolve()
+			const handleScan=(scan: UserScanDbRecord)=>{
+				const updatedScan=this.addUserItemsToScan(
+					now,items,streamBoundary,
+					tx.objectStore(type),scan
+				)
+				tx.objectStore('userScans').put(updatedScan)
+			}
+			if (forceNewScan) {
+				handleScan(makeEmptyScan(uid,type,now))
+			} else {
+				const getScanRequest=tx.objectStore('userScans').get([uid,0,type])
+				getScanRequest.onsuccess=()=>{
+					if (getScanRequest.result==null) {
+						handleScan(makeEmptyScan(uid,type,now))
+					} else {
+						handleScan(getScanRequest.result)
+					}
+				}
+			}
+		})
+	}
+	private addUserItemsToScan<T extends keyof UserItemDbRecordMap>(
+		now: Date, items: UserItemDbRecordMap[T][], streamBoundary: StreamBoundary,
+		store: IDBObjectStore, scan: UserScanDbRecord
+	): UserScanDbRecord {
+		for (const item of items) {
+			store.put(item)
+			scan=addUserItemIdsAndDateToScan(scan,item.createdAt,[item.id])
+		}
+		if (streamBoundary.date) {
+			scan=addUserItemIdsAndDateToScan(scan,streamBoundary.date,[])
+		}
+		if (streamBoundary.isFinished) {
+			scan.endDate=now
+		} else {
+			delete scan.endDate
+		}
+		return scan
+	}
 	static open(host: string): Promise<ChangesetViewerDBWriter> {
 		return this.openWithType(host,idb=>new ChangesetViewerDBWriter(idb))
+	}
+}
+
+function makeEmptyScan(uid: number, type: keyof UserItemDbRecordMap, beginDate: Date): UserScanDbRecord {
+	return {
+		uid,
+		type,
+		stash: 0,
+		items: {count:0},
+		beginDate,
+		empty: true
 	}
 }
 
--- src/worker/index.ts
+++ src/worker/index.ts
@@ -160,7 +160,7 @@ self.onconnect=ev=>{
 			await hostDataEntry.db.putUser(user)
 			if (changesetStream) {
 				const changesets=changesetsApiData.map(convertChangesetApiDataToDbRecord)
-				const restartedScan=await hostDataEntry.db.addUserItems('changesets',user.id,now,changesets,changesetStream.boundary,'toExistingScan')
+				const restartedScan=await hostDataEntry.db.addUserItemsIfNoScan('changesets',user.id,now,changesets,changesetStream.boundary)
 				if (restartedScan) {
 					hostDataEntry.userStreams.changesets.set(user.id,changesetStream)
 				}
@@ -223,8 +223,7 @@ async function scanUserItems<T extends 'changesets'|'notes'>(itemType: T, host: 
 	} else {
 		throw new RangeError(`unexpected item type`)
 	}
-	const mode=start?'toNewScan':'toNewOrExistingScan'
-	await hostDataEntry.db.addUserItems(itemType,uid,now,userItems,stream.boundary,mode)
+	await hostDataEntry.db.addUserItems(itemType,uid,now,userItems,stream.boundary,start)
 	hostDataEntry.broadcastSender.postOperationMessage({
 		type,uid,text,
 		status: 'ready'
