805ec95a03ffa230f64d1d35c25d2870eafd0717
Anton Khorev <tony29@yandex.ru>
2023-05-11 16:27:59 +0300

    Make grid item sequence module

--- src/grid/body.ts
+++ src/grid/body.ts
@@ -1,4 +1,6 @@
 import type {Server} from '../net'
+import type {ItemSequenceInfo} from './sequence'
+import {getItemSequenceInfo, writeItemSequenceInfo, readItemSequenceInfo, isGreaterItemSequenceInfo} from './sequence'
 import {
 	getItemCheckbox, markChangesetCellAsCombined, markChangesetCellAsUncombined,
 	makeUserCell, makeChangesetCell, makeNoteCell, makeCommentCell
@@ -12,60 +14,6 @@ type CellTimelineRelation = {
 	filled: boolean
 	withTimelineAbove: boolean
 	withTimelineBelow: boolean
-}
-
-type ItemSequenceInfo = {
-	timestamp: number
-	// rank: number
-	// type: MuxBatchItem['type'] | 'separator'
-	type: string,
-	id: number
-	order: number
-}
-
-function getItemTypeRank(type: ItemSequenceInfo['type']): number {
-	// 0 = rank of separators
-	switch (type) {
-	case 'user':
-		return 1
-	case 'changeset':
-		return 2
-	case 'changesetClose':
-		return 3
-	case 'note':
-		return 4
-	case 'changesetComment':
-		return 5
-	case 'noteComment':
-		return 6
-	}
-	return +Infinity
-}
-
-function readItemSequenceInfo($e: HTMLElement): ItemSequenceInfo {
-	return {
-		timestamp: Number($e.dataset.timestamp),
-		// rank: Number($e.dataset.rank),
-		type: $e.dataset.type??'',
-		id: Number($e.dataset.id),
-		order: Number($e.dataset.order)
-	}
-}
-
-function writeItemSequenceInfo($e: HTMLElement, info: ItemSequenceInfo): void {
-	$e.dataset.timestamp=String(info.timestamp)
-	// $e.dataset.rank=String(info.rank)
-	$e.dataset.rank=info.type
-	$e.dataset.id=String(info.id)
-	$e.dataset.order=String(info.order)
-}
-
-function isGreaterItemSequenceInfo(a: ItemSequenceInfo, b: ItemSequenceInfo): boolean {
-	if (a.timestamp>b.timestamp) return true
-	// if (a.rank>b.rank) return true
-	if (getItemTypeRank(a.type)>getItemTypeRank(b.type)) return true
-	if (a.id>b.id) return true
-	return a.order>b.order
 }
 
 export default class GridBody {
@@ -88,45 +36,12 @@ export default class GridBody {
 		batchItem: GridBatchItem,
 		usernames: Map<number, string>
 	): boolean {
-		const sequenceInfo=this.getItemSequenceInfo(batchItem)
+		const sequenceInfo=getItemSequenceInfo(batchItem)
 		if (!sequenceInfo) return false
 		const $item=this.renderExpandedItem(server,batchItem,usernames)
 		if (!$item) return false
 		this.addItem(columnHues,$item,batchItem.iColumns,sequenceInfo)
 		return true
-	}
-	private getItemSequenceInfo(
-		{type,item}: GridBatchItem
-	): ItemSequenceInfo|null {
-		let date: Date
-		let id: number
-		let order=0
-		if (type=='user') {
-			date=item.createdAt
-			id=item.id
-		} else if (type=='changeset' || type=='changesetClose') {
-			date=item.createdAt
-			if (type=='changesetClose' && item.closedAt) {
-				date=item.closedAt
-			}
-			id=item.id
-		} else if (type=='note') {
-			date=item.createdAt
-			id=item.id
-		} else if (type=='changesetComment' || type=='noteComment') {
-			date=item.createdAt
-			id=item.itemId
-			order=item.order
-		} else {
-			return null
-		}
-		return {
-			timestamp: date.getTime(),
-			// rank: getItemTypeRank(type),
-			type,
-			id,
-			order
-		}
 	}
 	private renderExpandedItem(
 		server: Server,
---
+++ src/grid/sequence.ts
@@ -0,0 +1,87 @@
+import type {GridBatchItem} from '../mux-user-item-db-stream-messenger'
+
+export type ItemSequenceInfo = {
+	timestamp: number
+	// rank: number
+	// type: MuxBatchItem['type'] | 'separator'
+	type: string,
+	id: number
+	order: number
+}
+
+function getItemTypeRank(type: ItemSequenceInfo['type']): number {
+	// 0 = rank of separators
+	switch (type) {
+	case 'user':
+		return 1
+	case 'changeset':
+		return 2
+	case 'changesetClose':
+		return 3
+	case 'note':
+		return 4
+	case 'changesetComment':
+		return 5
+	case 'noteComment':
+		return 6
+	}
+	return +Infinity
+}
+
+export function getItemSequenceInfo({type,item}: GridBatchItem): ItemSequenceInfo|null {
+	let date: Date
+	let id: number
+	let order=0
+	if (type=='user') {
+		date=item.createdAt
+		id=item.id
+	} else if (type=='changeset' || type=='changesetClose') {
+		date=item.createdAt
+		if (type=='changesetClose' && item.closedAt) {
+			date=item.closedAt
+		}
+		id=item.id
+	} else if (type=='note') {
+		date=item.createdAt
+		id=item.id
+	} else if (type=='changesetComment' || type=='noteComment') {
+		date=item.createdAt
+		id=item.itemId
+		order=item.order
+	} else {
+		return null
+	}
+	return {
+		timestamp: date.getTime(),
+		// rank: getItemTypeRank(type),
+		type,
+		id,
+		order
+	}
+}
+
+export function readItemSequenceInfo($e: HTMLElement): ItemSequenceInfo {
+	return {
+		timestamp: Number($e.dataset.timestamp),
+		// rank: Number($e.dataset.rank),
+		type: $e.dataset.type??'',
+		id: Number($e.dataset.id),
+		order: Number($e.dataset.order)
+	}
+}
+
+export function writeItemSequenceInfo($e: HTMLElement, info: ItemSequenceInfo): void {
+	$e.dataset.timestamp=String(info.timestamp)
+	// $e.dataset.rank=String(info.rank)
+	$e.dataset.type=info.type
+	$e.dataset.id=String(info.id)
+	$e.dataset.order=String(info.order)
+}
+
+export function isGreaterItemSequenceInfo(a: ItemSequenceInfo, b: ItemSequenceInfo): boolean {
+	if (a.timestamp>b.timestamp) return true
+	// if (a.rank>b.rank) return true
+	if (getItemTypeRank(a.type)>getItemTypeRank(b.type)) return true
+	if (a.id>b.id) return true
+	return a.order>b.order
+}
