9ce8ff5dc86926474426c2dab4afa88a096171d9
Anton Khorev <tony29@yandex.ru>
2023-05-17 02:31:02 +0300

    Refactor test code

--- test/src/grid/body.js
+++ test/src/grid/body.js
@@ -16,6 +16,99 @@ const usernames=new Map([
 	[102, `User Two`],
 ])
 
+const changesetBatchItem={
+	iColumns: [0],
+	type: 'changeset',
+	item: {
+		id: 10001,
+		uid: 101,
+		createdAt: new Date('2023-03'),
+		tags: {},
+		closedAt: new Date('2023-03'),
+		comments: {count:0},
+		changes: {count:1},
+		bbox: {
+			minLat: 60.0,
+			minLon: 30.0,
+			maxLat: 60.1,
+			maxLon: 30.1,
+		},
+	},
+}
+
+describe("GridBody",()=>{
+	const globalProperties=[
+		'document',
+		'HTMLElement',
+		'HTMLButtonElement',
+		'HTMLInputElement',
+	]
+	beforeEach(function(){
+		const jsdom=new JSDOM()
+		this.window=jsdom.window
+		for (const property of globalProperties) {
+			global[property]=jsdom.window[property]
+		}
+	})
+	afterEach(function(){
+		for (const property of globalProperties) {
+			delete global[property]
+		}
+	})
+	it("creates empty table body",()=>{
+		const nColumns=1
+		const getColumnHues=()=>new Array(nColumns).fill(0)
+		const gridBody=new GridBody(
+			server,null,getColumnHues
+		)
+		gridBody.setColumns(nColumns)
+		assert.equal(gridBody.$gridBody.rows.length,0)
+	})
+	it("adds one expanded item",()=>{
+		const nColumns=1
+		const getColumnHues=()=>new Array(nColumns).fill(0)
+		const gridBody=new GridBody(
+			server,null,getColumnHues
+		)
+		gridBody.setColumns(nColumns)
+		gridBody.addItem(changesetBatchItem,usernames,true)
+		gridBody.updateTableAccordingToSettings(false,false)
+		assertEach(gridBody.$gridBody.rows,$row=>{
+			assertRowIsSeparator($row)
+			assertSeparatorData($row,2023,3)
+		},$row=>{
+			assertRowIsItem($row)
+			assertElementClassType($row,'changeset')
+			assertItemData($row,Date.parse('2023-03'),'changeset',10001)
+		})
+	})
+	it("adds one collapsed item",()=>{
+		const nColumns=1
+		const getColumnHues=()=>new Array(nColumns).fill(0)
+		const gridBody=new GridBody(
+			server,null,getColumnHues
+		)
+		gridBody.setColumns(nColumns)
+		gridBody.addItem(changesetBatchItem,usernames,false)
+		gridBody.updateTableAccordingToSettings(false,false)
+		assertEach(gridBody.$gridBody.rows,$row=>{
+			assertRowIsSeparator($row)
+			assertSeparatorData($row,2023,3)
+		},$row=>{
+			assertRowIsCollection($row)
+			assertEach($row.cells,$cell=>{
+				assertEach($cell.children,$child=>{
+					assertCellChildIsIcon($child)
+				},$child=>{
+					assertCellChildIsItem($child)
+					assertElementClassType($child,'changeset')
+					assertItemData($child,Date.parse('2023-03'),'changeset',10001)
+				})
+			})
+		})
+	})
+})
+
 function assertElementClassType($e,classType) {
 	const classTypes=[
 		'changeset','note','comment'
@@ -75,110 +168,3 @@ function assertEach(xs,...fns) {
 		fn(xs[i])
 	}
 }
-
-describe("GridBody",()=>{
-	const globalProperties=[
-		'document',
-		'HTMLElement',
-		'HTMLButtonElement',
-		'HTMLInputElement',
-	]
-	beforeEach(function(){
-		const jsdom=new JSDOM()
-		this.window=jsdom.window
-		for (const property of globalProperties) {
-			global[property]=jsdom.window[property]
-		}
-	})
-	afterEach(function(){
-		for (const property of globalProperties) {
-			delete global[property]
-		}
-	})
-	it("creates empty table body",()=>{
-		const nColumns=1
-		const getColumnHues=()=>new Array(nColumns).fill(0)
-		const gridBody=new GridBody(
-			server,null,getColumnHues
-		)
-		gridBody.setColumns(nColumns)
-		assert.equal(gridBody.$gridBody.rows.length,0)
-	})
-	it("adds one expanded item",()=>{
-		const nColumns=1
-		const getColumnHues=()=>new Array(nColumns).fill(0)
-		const gridBody=new GridBody(
-			server,null,getColumnHues
-		)
-		gridBody.setColumns(nColumns)
-		gridBody.addItem({
-			iColumns: [0],
-			type: 'changeset',
-			item: {
-				id: 10001,
-				uid: 101,
-				createdAt: new Date('2023-03'),
-				tags: {},
-				closedAt: new Date('2023-03'),
-				comments: {count:0},
-				changes: {count:1},
-				bbox: {
-					minLat: 60.0,
-					minLon: 30.0,
-					maxLat: 60.1,
-					maxLon: 30.1,
-				},
-			},
-		},usernames,true)
-		assertEach(gridBody.$gridBody.rows,$row=>{
-			assertRowIsSeparator($row)
-			assertSeparatorData($row,2023,3)
-		},$row=>{
-			assertRowIsItem($row)
-			assertElementClassType($row,'changeset')
-			assertItemData($row,Date.parse('2023-03'),'changeset',10001)
-		})
-	})
-	it("adds one collapsed item",()=>{
-		const nColumns=1
-		const getColumnHues=()=>new Array(nColumns).fill(0)
-		const gridBody=new GridBody(
-			server,null,getColumnHues
-		)
-		gridBody.setColumns(nColumns)
-		gridBody.addItem({
-			iColumns: [0],
-			type: 'changeset',
-			item: {
-				id: 10001,
-				uid: 101,
-				createdAt: new Date('2023-03'),
-				tags: {},
-				closedAt: new Date('2023-03'),
-				comments: {count:0},
-				changes: {count:1},
-				bbox: {
-					minLat: 60.0,
-					minLon: 30.0,
-					maxLat: 60.1,
-					maxLon: 30.1,
-				},
-			},
-		},usernames,false)
-		assertEach(gridBody.$gridBody.rows,$row=>{
-			assertRowIsSeparator($row)
-			assertSeparatorData($row,2023,3)
-		},$row=>{
-			assertRowIsCollection($row)
-			assertEach($row.cells,$cell=>{
-				assertEach($cell.children,$child=>{
-					assertCellChildIsIcon($child)
-				},$child=>{
-					assertCellChildIsItem($child)
-					assertElementClassType($child,'changeset')
-					assertItemData($child,Date.parse('2023-03'),'changeset',10001)
-				})
-			})
-		})
-	})
-})
