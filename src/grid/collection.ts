c9412c8738b29159ed000beddc68ecab22499ccd
Anton Khorev <tony29@yandex.ru>
2023-05-21 05:39:39 +0300

    Add collection-sequence point compare method

--- src/grid/collection.ts
+++ src/grid/collection.ts
@@ -1,4 +1,5 @@
 import type {ItemSequencePoint} from './info'
+import {readItemSequencePoint, isGreaterElementSequencePoint} from './info'
 
 export default class GridBodyCollectionRow {
 	constructor(private $row: HTMLTableRowElement) {}
@@ -6,7 +7,22 @@ export default class GridBodyCollectionRow {
 	 * Check if all collection items are greater than the given sequence point (1), all are less than it (-1), some are greater and some are less (0)
 	 */
 	compare(sequencePoint: ItemSequencePoint): -1|0|1 {
-		return 0 // TODO
+		let hasGreaterItem=false
+		let hasLesserItem=false
+		for (const $cell of this.$row.cells) {
+			for (const $item of $cell.children) {
+				if (!($item instanceof HTMLElement) || !$item.classList.contains('item')) continue
+				const collectionItemSequencePoint=readItemSequencePoint($item)
+				if (!collectionItemSequencePoint) continue
+				if (isGreaterElementSequencePoint(collectionItemSequencePoint,sequencePoint)) {
+					hasGreaterItem=true
+				} else {
+					hasLesserItem=true
+				}
+				if (hasGreaterItem && hasLesserItem) return 0
+			}
+		}
+		return hasGreaterItem?1:-1
 	}
 	/**
 	 * Split collection into two rows at the given sequence point
---
+++ test/src/grid/collection.js
@@ -0,0 +1,119 @@
+import {strict as assert} from 'assert'
+import {JSDOM} from 'jsdom'
+import GridBodyCollectionRow from '../../../test-build/grid/collection.js'
+
+function row(contents) {
+	const $row=document.createElement('tr')
+	$row.classList.add('collection')
+	$row.innerHTML=contents
+	return $row
+}
+function cell(contents) {
+	if (!contents) return `<td></td>`
+	return `<td><span class="icon"></span>${contents}</td>`
+}
+function changeset(date,id) {
+	return `<span class="item changeset combined" data-timestamp="${Date.parse(date)}" data-type="changeset" data-id="${id}"></span>`
+}
+
+describe("GridBodyCollectionRow",()=>{
+	const globalProperties=[
+		'document',
+		'HTMLElement',
+	]
+	beforeEach(function(){
+		const jsdom=new JSDOM()
+		this.window=jsdom.window
+		for (const property of globalProperties) {
+			global[property]=jsdom.window[property]
+		}
+	})
+	afterEach(function(){
+		for (const property of globalProperties) {
+			delete global[property]
+		}
+	})
+	it("compares empty collection as lesser",()=>{
+		const $row=row(cell())
+		const collection=new GridBodyCollectionRow($row)
+		const cmp=collection.compare({
+			timestamp: Date.parse('2023-04-06'),
+			type: 'changeset',
+			id: 10001,
+		})
+		assert.equal(cmp,-1)
+	})
+	it("compares single-element collection as greater",()=>{
+		const $row=row(cell(changeset('2023-05-07',10101)))
+		const collection=new GridBodyCollectionRow($row)
+		const cmp=collection.compare({
+			timestamp: Date.parse('2023-04-06'),
+			type: 'changeset',
+			id: 10001,
+		})
+		assert.equal(cmp,1)
+	})
+	it("compares single-element collection as lesser",()=>{
+		const $row=row(cell(changeset('2023-03-05',9901)))
+		const collection=new GridBodyCollectionRow($row)
+		const cmp=collection.compare({
+			timestamp: Date.parse('2023-04-06'),
+			type: 'changeset',
+			id: 10001,
+		})
+		assert.equal(cmp,-1)
+	})
+	it("compares 2-element collection as greater",()=>{
+		const $row=row(cell(
+			changeset('2023-05-08',10102)+
+			changeset('2023-05-07',10101)
+		))
+		const collection=new GridBodyCollectionRow($row)
+		const cmp=collection.compare({
+			timestamp: Date.parse('2023-04-06'),
+			type: 'changeset',
+			id: 10001,
+		})
+		assert.equal(cmp,1)
+	})
+	it("compares 2-element collection as neither",()=>{
+		const $row=row(cell(
+			changeset('2023-05-09',10103)+
+			changeset('2023-05-07',10101)
+		))
+		const collection=new GridBodyCollectionRow($row)
+		const cmp=collection.compare({
+			timestamp: Date.parse('2023-05-08'),
+			type: 'changeset',
+			id: 10102,
+		})
+		assert.equal(cmp,0)
+	})
+	it("compares 2-column 2-element collection as neither",()=>{
+		const $row=row(
+			cell(changeset('2023-05-09',10103))+
+			cell(changeset('2023-05-07',10101))
+		)
+		const collection=new GridBodyCollectionRow($row)
+		const cmp=collection.compare({
+			timestamp: Date.parse('2023-05-08'),
+			type: 'changeset',
+			id: 10102,
+		})
+		assert.equal(cmp,0)
+	})
+	it("compares 2-element collection with empty cell as lesser",()=>{
+		const $row=row(
+			cell(changeset('2023-05-09',10103))+
+			cell()+
+			cell(changeset('2023-05-07',10101))
+		)
+		const collection=new GridBodyCollectionRow($row)
+		const cmp=collection.compare({
+			timestamp: Date.parse('2023-05-10'),
+			type: 'changeset',
+			id: 10110,
+		})
+		assert.equal(cmp,-1)
+	})
+})
