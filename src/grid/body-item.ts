8399fcb246ca75b4d3841b06ba975f144e6cdce1
Anton Khorev <tony29@yandex.ru>
2023-06-01 03:06:27 +0300

    Move editor data to its own module

--- src/grid/body-item.ts
+++ src/grid/body-item.ts
@@ -1,4 +1,5 @@
 import {getHueFromUid} from './colorizer'
+import editorData from './editors'
 import {makeDateOutput} from '../date'
 import type {MuxBatchItem} from '../mux-user-item-db-stream'
 import {makeElement, makeLink} from '../util/html'
@@ -145,22 +146,6 @@ export function writeCollapsedItemFlow(
 		)
 	}
 }
-
-const editorData:[editorId:string,createdByPrefix:string,url:string][]=[
-	['everydoor','Every Door','https://wiki.openstreetmap.org/wiki/Every_Door'],
-	['gomap','Go Map!!','https://wiki.openstreetmap.org/wiki/Go_Map!!'],
-	['id','iD','https://wiki.openstreetmap.org/wiki/ID'],
-	['josm','JOSM','https://wiki.openstreetmap.org/wiki/JOSM'],
-	['mapbuilder','Map builder','https://www.bing.com/mapbuilder/'],
-	['mapcomplete','MapComplete','https://wiki.openstreetmap.org/wiki/MapComplete'],
-	['mapsme','MAPS.ME','https://wiki.openstreetmap.org/wiki/MAPS.ME'],
-	['organicmaps','Organic Maps','https://wiki.openstreetmap.org/wiki/Organic_Maps'],
-	['osmand','OsmAnd','https://wiki.openstreetmap.org/wiki/OsmAnd'],
-	['potlatch','Potlatch','https://wiki.openstreetmap.org/wiki/Potlatch'],
-	['rapid','RapiD','https://wiki.openstreetmap.org/wiki/Rapid'],
-	['streetcomplete','StreetComplete','https://wiki.openstreetmap.org/wiki/StreetComplete'],
-	['vespucci','Vespucci','https://wiki.openstreetmap.org/wiki/Vespucci'],
-]
 
 export function writeExpandedItemFlow(
 	$flow: HTMLElement,
---
+++ src/grid/editors.ts
@@ -0,0 +1,17 @@
+const editorData:[editorId:string,createdByPrefix:string,url:string][]=[
+	['everydoor','Every Door','https://wiki.openstreetmap.org/wiki/Every_Door'],
+	['gomap','Go Map!!','https://wiki.openstreetmap.org/wiki/Go_Map!!'],
+	['id','iD','https://wiki.openstreetmap.org/wiki/ID'],
+	['josm','JOSM','https://wiki.openstreetmap.org/wiki/JOSM'],
+	['mapbuilder','Map builder','https://www.bing.com/mapbuilder/'],
+	['mapcomplete','MapComplete','https://wiki.openstreetmap.org/wiki/MapComplete'],
+	['mapsme','MAPS.ME','https://wiki.openstreetmap.org/wiki/MAPS.ME'],
+	['organicmaps','Organic Maps','https://wiki.openstreetmap.org/wiki/Organic_Maps'],
+	['osmand','OsmAnd','https://wiki.openstreetmap.org/wiki/OsmAnd'],
+	['potlatch','Potlatch','https://wiki.openstreetmap.org/wiki/Potlatch'],
+	['rapid','RapiD','https://wiki.openstreetmap.org/wiki/Rapid'],
+	['streetcomplete','StreetComplete','https://wiki.openstreetmap.org/wiki/StreetComplete'],
+	['vespucci','Vespucci','https://wiki.openstreetmap.org/wiki/Vespucci'],
+]
+
+export default editorData
