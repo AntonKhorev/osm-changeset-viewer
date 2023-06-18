0ab9178d12d0f18c5fab39dd68a8155f5fc3f6ed
Anton Khorev <tony29@yandex.ru>
2023-06-19 02:17:52 +0300

    Add date and id options

--- src/grid/item-options.ts
+++ src/grid/item-options.ts
@@ -5,17 +5,23 @@ type ItemOptionsEntry = {
 }
 
 export default class ItemOptions {
+	date    : boolean
+	id      : boolean
 	api     : boolean
 	editor  : boolean
 	source  : boolean
 	comments: boolean
 	constructor(isExpanded: boolean) {
 		if (isExpanded) {
+			this.date    =true
+			this.id      =true
 			this.api     =true
-			this.editor =true
-			this.source =true
+			this.editor  =true
+			this.source  =true
 			this.comments=true
 		} else {
+			this.date    =false
+			this.id      =true
 			this.api     =false
 			this.editor  =false
 			this.source  =false
@@ -24,6 +30,8 @@ export default class ItemOptions {
 	}
 	list(): ItemOptionsEntry[] {
 		return [
+			{ label: 'date'    , get: ()=>this.date    , set: v=>this.date    =v },
+			{ label: 'id'      , get: ()=>this.id      , set: v=>this.id      =v },
 			{ label: 'api'     , get: ()=>this.api     , set: v=>this.api     =v },
 			{ label: 'editor'  , get: ()=>this.editor  , set: v=>this.editor  =v },
 			{ label: 'source'  , get: ()=>this.source  , set: v=>this.source  =v },
