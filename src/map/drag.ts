4bfca0c7f2a77d8b2cbb5e462a6811ecae41d986
Anton Khorev <tony29@yandex.ru>
2023-07-12 22:02:23 +0300

    Prepare for map fling animation

--- src/map/drag.ts
+++ src/map/drag.ts
@@ -1,9 +1,16 @@
+const speedDecayRate=0.003
+
 type Grab = {
 	pointerId: number
 	startViewPxOffsetX: number
 	startViewPxOffsetY: number
 	startPxX: number
 	startPxY: number
+	currentTime: number
+	currentPxX: number
+	currentPxY: number
+	currentSpeedPxX: number
+	currentSpeedPxY: number
 }
 
 export default function installDragListeners(
@@ -22,21 +29,36 @@ export default function installDragListeners(
 			pointerId: ev.pointerId,
 			startViewPxOffsetX: ev.clientX,
 			startViewPxOffsetY: ev.clientY,
-			startPxX,startPxY
+			startPxX,startPxY,
+			currentTime: performance.now(),
+			currentPxX: startPxX,
+			currentPxY: startPxY,
+			currentSpeedPxX: 0,
+			currentSpeedPxY: 0,
 		}
 		$mapView.setPointerCapture(ev.pointerId)
 		$mapView.classList.add('grabbed')
 	}
 	$mapView.onpointerup=$mapView.onpointercancel=ev=>{
 		if (!grab || grab.pointerId!=ev.pointerId) return
+		console.log('> fling speed',grab.currentSpeedPxX,grab.currentSpeedPxY) ///
 		grab=undefined
 		$mapView.classList.remove('grabbed')
 	}
 	$mapView.onpointermove=ev=>{
 		if (!grab || grab.pointerId!=ev.pointerId) return
-		pan(
-			grab.startPxX-(ev.clientX-grab.startViewPxOffsetX),
-			grab.startPxY-(ev.clientY-grab.startViewPxOffsetY)
-		)
+		const newPxX=grab.startPxX-(ev.clientX-grab.startViewPxOffsetX)
+		const newPxY=grab.startPxY-(ev.clientY-grab.startViewPxOffsetY)
+		const newTime=performance.now()
+		const dx=newPxX-grab.currentPxX
+		const dy=newPxY-grab.currentPxY
+		const dt=newTime-grab.currentTime
+		const speedDecay=Math.exp(-speedDecayRate*dt)
+		grab.currentSpeedPxX=grab.currentSpeedPxX*speedDecay+dx/dt*(1-speedDecay)
+		grab.currentSpeedPxY=grab.currentSpeedPxY*speedDecay+dy/dt*(1-speedDecay)
+		grab.currentPxX=newPxX
+		grab.currentPxY=newPxY
+		grab.currentTime=newTime
+		pan(newPxX,newPxY)
 	}
 }
