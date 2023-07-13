ce02ca15dd1a139f133a281da8fdbdcf7b9cbaf3
Anton Khorev <tony29@yandex.ru>
2023-07-13 03:43:57 +0300

    Fix map tile layer boundary

--- src/map/layer/tile.ts
+++ src/map/layer/tile.ts
@@ -1,7 +1,7 @@
 import type {RenderView} from './base'
 import Layer from './base'
 import {TileProvider} from '../../net'
-import {tilePxSize, calculatePxSize} from '../geo'
+import {tilePxSize} from '../geo'
 import {makeElement} from '../../util/html'
 
 export default class TileLayer extends Layer {
@@ -12,26 +12,21 @@ export default class TileLayer extends Layer {
 	}
 	render(view: RenderView): void {
 		this.$layer.replaceChildren()
-		// const viewTileX1=Math.floor(view.pxX1/tilePxSize)
-		// const viewTileX2=Math.ceil(view.pxX2/tilePxSize)
-		// const viewTileY1=Math.floor(view.pxY1/tilePxSize)
-		// const viewTileY2=Math.ceil(view.pxY2/tilePxSize)
-		const pxSize=calculatePxSize(view.z)
-		for (let pxY=view.pxY1;pxY<view.pxY2;pxY+=tilePxSize) {
-			const y=pxY*pxSize
-			if (y<0 || y>=1) continue
-			const tileY=Math.floor(pxY/tilePxSize)
+		const viewTileX1=Math.floor(view.pxX1/tilePxSize)
+		const viewTileX2=Math.ceil(view.pxX2/tilePxSize)
+		const viewTileY1=Math.floor(view.pxY1/tilePxSize)
+		const viewTileY2=Math.ceil(view.pxY2/tilePxSize)
+		const tileMask=2**view.z-1
+		for (let tileY=viewTileY1;tileY<viewTileY2;tileY++) {
+			if (tileY<0 || tileY>tileMask) continue
 			const tileOffsetPxY=tileY*tilePxSize-view.pxY1
-			for (let pxX=view.pxX1;pxX<view.pxX2;pxX+=tilePxSize) {
-				const x=pxX*pxSize
-				const tileX=Math.floor((x-Math.floor(x))/pxSize/tilePxSize)
-				const repeatedTileX=Math.floor(pxX/tilePxSize)
-				const tileOffsetPxX=repeatedTileX*tilePxSize-view.pxX1
+			for (let tileX=viewTileX1;tileX<viewTileX2;tileX++) {
+				const tileOffsetPxX=tileX*tilePxSize-view.pxX1
 				const $img=makeElement('img')()()
 				$img.src=this.tileProvider.urlTemplate
 					.replace('{z}',String(view.z))
-					.replace('{x}',String(tileX))
-					.replace('{y}',String(tileY))
+					.replace('{x}',String(tileX&tileMask))
+					.replace('{y}',String(tileY&tileMask))
 				$img.style.translate=`${tileOffsetPxX}px ${tileOffsetPxY}px`
 				this.$layer.append($img)
 			}
