dd81e26f7c1a72f98e1d954ae46218c5dfc56d73
Anton Khorev <tony29@yandex.ru>
2023-07-14 12:18:29 +0300

    Change hue picker value with left/right keys

--- src/grid/hue-picker.ts
+++ src/grid/hue-picker.ts
@@ -9,12 +9,38 @@ export function makeHuePicker() {
 	$stripe.style.background=`linear-gradient(to right, ${stripeStops.join(', ')})`
 	const $picker=makeDiv('hue-picker')($stripe)
 	$picker.tabIndex=0
+	$picker.dataset.value=String(0)
+	$picker.onkeydown=ev=>{
+		const value=readValueFromString($picker.dataset.value??'')
+		let newValue:number|undefined
+		if (ev.key=='ArrowLeft') {
+			newValue=mod(value-10,360)
+		} else if (ev.key=='ArrowRight') {
+			newValue=mod(value+10,360)
+		}
+		if (newValue!=null) {
+			updateHuePicker($picker,newValue)
+			ev.stopPropagation()
+			ev.preventDefault()
+		}
+	}
 	return $picker
 }
 
-export function updateHuePicker($huePicker: HTMLElement, hue: number) {
-	const $stripe=$huePicker.querySelector(':scope > .hue-picker-stripe')
+export function updateHuePicker($picker: HTMLElement, value: number) {
+	$picker.dataset.value=String(value)
+	const $stripe=$picker.querySelector(':scope > .hue-picker-stripe')
 	if ($stripe instanceof HTMLElement) {
-		$stripe.style.left=`${-hue*100/360}%`
+		$stripe.style.left=`${-value*100/360}%`
 	}
+}
+
+function readValueFromString(s: string): number {
+	const v=Number(s)
+	if (!Number.isFinite(v)) return 0
+	return mod(Math.round(v),360)
+}
+
+function mod(n: number, d: number): number {
+	return (n%d+d)%d
 }
